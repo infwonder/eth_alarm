108e920db1fe2d3a2240ae2bcedb62ed6eff1076
Yu-Cheng Lin <infwonder@gmail.com>
2017-08-22 00:17:33 -0600

    test new layout. adding both block number or block timestamp trigger types

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 960, height: 680})
+  win = new BrowserWindow({width: 1280, height: 960})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
