9b3d9b649b503249dbfd74c817a63ab03cd7ec0a
Jason Lin <jasonlin@eve.home>
2017-10-03 19:41:15 -0600

    backed up to pkgs

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1280, height: 960})
+  win = new BrowserWindow({width: 1280, height: 960});
+  win.setMenu(null);
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +19,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
